30d4b69ebbf022978c2383943000a3cd608aeaa2
diogonzafe <digadiogooh@gmail.com>
2024-12-09 23:48:38 -0300

    docker and postgres configuration

---
+++ Documents/CryptoAlertsProject/CryptoAlerts/src/main/java/org/diogodev/MainApplication.java
@@ -0,0 +1,15 @@
+package org.diogodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan("org.diogodev")
+@EnableJpaRepositories(basePackages = "org.diogodev")
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
